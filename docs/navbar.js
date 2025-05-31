48b898ad715a81827e96b50e4f4d291d4f8f0214
DBM <diegobarmor42@gmail.com>
2025-05-31 17:41:25 +0200

    improved navbar

--- docs/navbar.js
+++ docs/navbar.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     var selectElement = document.getElementById("page-select");
+    var currentPage = window.location.pathname.split("/").pop().replace(".html", "");
+
+    // Set the dropdown to the current page
+    for (var i = 0; i < selectElement.options.length; i++) {
+        if (selectElement.options[i].value == currentPage) {
+            selectElement.selectedIndex = i;
+            selectElement.title = selectElement.options[i].text;
+            break;
+        }
+    }
+
     selectElement.addEventListener("change", function() {
-        window.location.href = "https://diegobarmor.github.io/thesis-qcb/" + selectElement.value + ".html";
+        var selectedValue = selectElement.value;
+        if (selectedValue == currentPage) return;
+        selectElement.title = selectElement.options[selectElement.selectedIndex].text;
+        window.location.href = "https://diegobarmor.github.io/thesis-qcb/" + selectedValue + ".html";
     });
 });
